d6df2f2a376feefe1616c35bbe9f5fa773828d9f
omkarchorghe16 <omkarchorghe16@gmail.com>
2023-11-06 23:53:36 -0500

    some comments

---
+++ src/main/java/com/example/SpringBootIntelij/SpringBootIntelijApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootIntelij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntelijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntelijApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootIntelij/SpringBootVsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootIntelij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
