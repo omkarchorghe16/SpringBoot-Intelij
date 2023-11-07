932c25faf6551429a39417a47dbbc1b045f40c47
omkarchorghe16 <omkarchorghe16@gmail.com>
2023-11-07 00:28:29 -0500

    added logger

--- src/main/java/com/example/SpringBootIntelij/SpringBootIntelijApplication.java
+++ src/main/java/com/example/SpringBootIntelij/SpringBootIntelijApplication.java
@@ -1,5 +1,7 @@
 package com.example.SpringBootIntelij;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootIntelijApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootIntelijApplication.class, args);
+		System.out.println("Welcome to Intelij IDE");
 	}
 
 }
